79c785db8140e9a6c56537de78ab8478d22751d3
Nelen Maharjan <neleen11@gmail.com>
2025-02-11 21:53:01 +0545

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,33 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        rubik: ['Rubik-Regular', 'san-serif'],
+        "rubik-bold": ['Rubik-Bold', 'san-serif'],
+        "rubik-extrabold": ['Rubik-ExtraBold', 'san-serif'],
+        "rubik-medium": ['Rubik-Medium', 'san-serif'],
+        "rubik-semibold": ['Rubik-SemiBold', 'san-serif'],
+        "rubik-light": ['Rubik-Light', 'san-serif'],
+      },
+      colors:{
+        primary:{
+          100: '#0061FF0A',
+        200: '#0061FF1A',
+        300: '#0061FF',
+        },
+        accent:{
+          100: 'FBFBFD'
+        },
+        black:{
+          DEFAULT: '#000000',
+          100: '#8C8E98',
+          200: '#666876',
+          300: '#191D31',
+        },
+        danger: '#F75555'
+      }
+    },
   },
   plugins: [],
 }
