d08834069ecb1d89b8ce4289d6c6d2e337924b0b
Nelen Maharjan <neleen11@gmail.com>
2025-02-11 21:29:13 +0545

    layout update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,27 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import './globals.css';
+import {useFonts} from 'expo-font';
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "Rubic-Bold": require('../assets/fonts/Rubic-Bold.ttf'),
+    "Rubic-ExtraBold": require('../assets/fonts/Rubik-ExtraBold.ttf'),
+    "Rubic-Light": require('../assets/fonts/Rubik-Light.ttf'),
+    "Rubic-Medium": require('../assets/fonts/Rubik-Medium.ttf'),
+    "Rubic-Regular": require('../assets/fonts/Rubik-Regular.ttf'),
+    "Rubic-SemiBold": require('../assets/fonts/Rubik-SemiBold.ttf'),
+  });
+  
+  useEffect(()=>{
+    if(fontsLoaded){
+      SplashScreen.hideAsync();
+    }
+  },[fontsLoaded]);
+
+  if(!fontsLoaded){
+    return null;
+  }
+  
   return <Stack />;
 }
