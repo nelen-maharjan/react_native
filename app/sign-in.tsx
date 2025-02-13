ebdd5fce5f8adf0adac49668b0a2c3c47ee6f63f
Nelen Maharjan <neleen11@gmail.com>
2025-02-13 21:06:29 +0545

    sign-in

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className='h-full bg-white'>
+      <ScrollView>
+        
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
