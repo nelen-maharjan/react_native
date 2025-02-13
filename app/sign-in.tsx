1e8f68a9f208524d65e2309a4c2654e85182d05e
Nelen Maharjan <neleen11@gmail.com>
2025-02-13 21:24:46 +0545

    sign-in ui complete

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -13,7 +13,7 @@ const SignIn = () => {
   return (
     <SafeAreaView className='h-full bg-white'>
       <ScrollView contentContainerClassName='h-full'>
-        <Image source={images.onboarding} className='w-full h-4/6' resizeMode='contain' />
+        <Image source={images.onboarding} className='w-full h-3/6' resizeMode='contain' />
         <View className='px-10'>
           <Text className='text-base text-center uppercase font-rubik text-black-200'>Welcome To RealState</Text>
           <Text className='text-3xl font=rubik-bold text-black-300 text-center mt-2'>
@@ -26,11 +26,14 @@ const SignIn = () => {
           </Text>
 
           <TouchableOpacity onPress={handleLogin} className='shadow-md bg-white shadow-zinc-300 rounded-full w-full py-4 mt-5'>
+            <View className='flex flex-row items-center justify-center'>
             <Image
-            src={icons.google}
+            source={icons.google}
             className='w-5 h-5'
             resizeMode='contain'
             />
+            <Text className='text-lg font-rubik-medium text-black-300 ml-2'>Continue with Google</Text>
+            </View>
           </TouchableOpacity>
         </View>
       </ScrollView>
