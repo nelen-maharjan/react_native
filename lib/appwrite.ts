4b6d3b40dbe246d9f2da4230f0548dd276599b9d
Nelen Maharjan <neleen11@gmail.com>
2025-02-14 21:57:27 +0545

    logout

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -53,3 +53,13 @@ export async function login() {
     return false;
   }
 }
+
+export async function logout(){
+    try {
+        await account.deleteSession('current');
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
