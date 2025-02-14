8777634453100ef0b5bfd27871d15a3cd4240342
Nelen Maharjan <neleen11@gmail.com>
2025-02-14 21:38:37 +0545

    login function

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,14 +1,34 @@
-import {Client} from 'react-native-appwrite';
+import { Account, Avatars, Client, OAuthProvider } from "react-native-appwrite";
+import * as Linking from "expo-linking";
 
 export const config = {
-    platform: 'com.react-native-crash-course',
-    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-}
+  platform: "com.react-native-crash-course",
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+};
 
 export const client = new Client();
 
 client
-    .setEndpoint(config.endpoint!)
-    .setProject(config.projectId!)
-    .setPlatform(config.platform!)
+  .setEndpoint(config.endpoint!)
+  .setProject(config.projectId!)
+  .setPlatform(config.platform!);
+
+export const avatar = new Avatars(client);
+export const account = new Account(client);
+
+export async function login() {
+  try {
+    const redirectUri = Linking.createURL("/");
+
+    const response = await account.createOAuth2Token(
+      OAuthProvider.Google,
+      redirectUri
+    );
+
+    if(!response) throw new Error('failed to login')
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
