c0f6c5ac4cc6ac46fcedb0755954709d4b0f3c15
Nelen Maharjan <neleen11@gmail.com>
2025-02-14 21:34:41 +0545

    appwrite config

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,3 +1,14 @@
+import {Client} from 'react-native-appwrite';
+
 export const config = {
-    
+    platform: 'com.react-native-crash-course',
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
 }
+
+export const client = new Client();
+
+client
+    .setEndpoint(config.endpoint!)
+    .setProject(config.projectId!)
+    .setPlatform(config.platform!)
