63e639b88e04669f983b905bca9b973b8ccf0628
Nelen Maharjan <neleen11@gmail.com>
2025-02-15 22:25:52 +0545

    AppLayout

--- lib/global-provider.tsx
+++ lib/global-provider.tsx
@@ -5,7 +5,7 @@ import { useAppwrite } from "./useAppwrite";
 import { Redirect } from "expo-router";
 
 interface GlobalContextType {
-  isLogged: boolean;
+  isLoggedIn: boolean;
   user: User | null;
   loading: boolean;
   refetch: () => void;
@@ -33,12 +33,14 @@ export const GlobalProvider = ({ children }: GlobalProviderProps) => {
     fn: getCurrentUser,
   });
 
-  const isLogged = !!user;
+  const isLoggedIn = !!user;
+//   console.log(JSON.stringify(user, null, 2));
+  
 
   return (
     <GlobalContext.Provider
       value={{
-        isLogged,
+        isLoggedIn,
         user,
         loading,
         refetch,
